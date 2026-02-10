878b335c7ef468f20f335a57d4f1084344058c29
Alt-Charlie <charlie@alt-design.net>
2026-02-10 11:50:41 +0000

    Statamic V6 Support

--- routes/cp.php
+++ routes/cp.php
@@ -2,7 +2,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['statamic.cp.authenticated'], 'namespace' => 'AltDesign\AltSeo\Http\Controllers'], function() {
+    
     // Settings
-    Route::get('/alt-design/alt-seo/', 'AltController@index')->name('alt-seo.index');
-    Route::post('/alt-design/alt-seo/', 'AltController@update')->name('alt-seo.update');
+    Route::group(['middleware' => ['can:view alt-seo']], function () {
+        Route::get('/alt-design/alt-seo/', 'AltController@index')->name('alt-seo.index');
+        Route::post('/alt-design/alt-seo/', 'AltController@update')->name('alt-seo.update');
+    });
+
+    // Statamic V6 redirect the Addon settings route
+    Route::get('/addons/alt-seo/settings', fn () => redirect()->to(cp_route('alt-seo.index')));
 });
